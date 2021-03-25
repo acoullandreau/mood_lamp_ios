a61268889a555d58b7a806bacd27ee5dc4a945de
Alexina Coullandreau <mozart38@gmail.com>
2021-03-25 16:09:43 +0100

    Initial Commit

---
+++ Maia/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Maia
+//
+//  Created by Alexina Coullandreau on 01/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var connectButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // format the connect button
+        connectButton.layer.cornerRadius = 8
+    }
+
+}
+
