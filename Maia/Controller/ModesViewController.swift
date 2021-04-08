0e1dfe30b685bfe40f946231bcdb87a49d14c15d
Alexina Coullandreau <mozart38@gmail.com>
2021-04-08 16:34:37 +0200

    Adding style to the segmented control Adding a view for each control, as well as the logic to switch from one view to the other using the segmented control

--- Maia/Controller/ModesViewController.swift
+++ Maia/Controller/ModesViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ModesViewController: UIViewController {
 
+    @IBOutlet weak var customModesView: UIView!
+    @IBOutlet weak var defaultModesView: UIView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // format the connect button
+        // format the segmented control
+        UISegmentedControl.appearance().backgroundColor = UIColor(red: 0.46, green: 0.46, blue: 0.50, alpha: 0.24)
+        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(red: 0.85, green: 0.81, blue: 0.85, alpha: 1.00)
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(named: "TextLightColor")!], for: .normal)
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(named: "TextDarkColor")!], for: .selected)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,6 +35,19 @@ class ModesViewController: UIViewController {
            self.performSegue(withIdentifier: "infoPopUp", sender: self)
         }
     }
+    
+    @IBAction func switchModesView(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            defaultModesView.alpha = 1
+            customModesView.alpha = 0
+        } else {
+            defaultModesView.alpha = 0
+            customModesView.alpha = 1
+        }
+        
+    }
+    
+    
     
 }
 
